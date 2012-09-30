d6f25d502852957217c8a84ed18c3f6741ce4285
dxiao <dxiao@mit.edu>
2012-09-30 12:56:30 -0400

    Added base library files

---
+++ Platformer/test/TestSlick.java
@@ -0,0 +1,39 @@
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.AppGameContainer;
+
+/**
+ * 
+ * @author dxiao
+ */
+public class TestSlick extends BasicGame {
+
+    public TestSlick () {
+        super("SimpleTest");
+    }
+    
+    @Override
+    public void init(GameContainer container) throws SlickException {}
+
+    @Override
+    public void update(GameContainer container, int delta)
+            throws SlickException {}
+
+    @Override
+    public void render(GameContainer container, Graphics g)
+            throws SlickException {
+        g.drawString("Hello, Slick world!", 0, 100);
+    }
+
+    public static void main(String[] args) {
+        try {
+            AppGameContainer app = new AppGameContainer(new TestSlick ());
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+}
