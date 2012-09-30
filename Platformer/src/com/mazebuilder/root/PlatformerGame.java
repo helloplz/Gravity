79872dfd75afc487c863dbe5c19dac6c60d0bd68
dxiao <dxiao@mit.edu>
2012-09-30 13:01:28 -0400

    Added src directory to repo

---
+++ Platformer/src/com/mazebuilder/root/PlatformerGame.java
@@ -0,0 +1,34 @@
+package com.mazebuilder.root;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * 
+ * @author dxiao
+ */
+public class PlatformerGame extends StateBasedGame {
+
+	public PlatformerGame() {
+		super("The Platformers v0.0");
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+	}
+
+	public static void main(String args[]) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new PlatformerGame());
+
+		app.setDisplayMode(1024, 768, false);
+		app.setMaximumLogicUpdateInterval(100);
+		app.setMinimumLogicUpdateInterval(10);
+		app.setTargetFrameRate(60);
+		// app.setSmoothDeltas(true);
+
+		app.start();
+
+	}
+}
