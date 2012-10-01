fe813b672bd074cb2630c5a1ed4b9a7e1e3f38e6
dxiao <dxiao@mit.edu>
2012-10-01 17:33:46 -0400

    Location implementation (basic)

--- Platformer/src/com/gravity/gameplay/Location.java
+++ Platformer/src/com/gravity/gameplay/Location.java
@@ -1,9 +1,50 @@
 package com.gravity.gameplay;
 
+public class Location {
 
-public interface Location {
+	private final float x;
+	private final float y;
 
-	float getX();
+	public Location(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
 
-	float getY();
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	@Override
+	public String toString() {
+		return "Location [x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
 }
