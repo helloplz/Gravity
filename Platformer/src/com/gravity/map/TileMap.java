8e140622f2a787e039a992552141d7f3f07ad86a
Chris Dessonville <cdville@mit.edu>
2012-10-03 01:33:57 -0400

    First take at implementing maps.  TileMap.update() remains to be done.

--- Platformer/src/com/gravity/map/TileMap.java
+++ Platformer/src/com/gravity/map/TileMap.java
@@ -7,35 +7,71 @@ import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.tiled.TiledMap;
 
 import com.gravity.player.Entity;
+import com.gravity.player.HumanPlayer;
 
 public class TileMap {
 
-	Tile[][] terrain;
-	List<Entity> entities;
-	Map<Entity, Vector2f> positions;
+    Tile[][] terrain;
+    List<Entity> entities;
+    Map<Entity, Vector2f> positions;
 
-	int height;
-	int width;
+    int height;
+    int width;
 
-	public TileMap(TiledMap map) {
-		// TODO: using tiledmap, populate terrain and entities
-	}
+    public TileMap(TiledMap map) {
+        // Get width/height
+        this.width = new Integer(map.getMapProperty("width", "1024"));
+        this.height = new Integer(map.getMapProperty("height", "1024"));
 
-	public void update() {
-		// TODO: for each entity, get the desired velocity, and calculate
-		// an actual velocity based on the entity's location, terrain, and
-		// gravity. Then calculate each entity's new position
-		for (Entity entity : entities) {
-			Vector2f desiredV = entity.update();
+        // Assume all objects are currently terrain
+        int groupCount = map.getObjectGroupCount();
+        for (int groupID = 0; groupID < groupCount; groupID++) {
+            int objectCount = map.getObjectCount(groupID);
+            for (int objectID = 0; objectID < objectCount; objectID++) {
+                String type = map.getObjectType(groupID, objectID);
 
-		}
-	}
+                if (type.equals("terrain")) {
+                    int x = map.getObjectX(groupID, objectID);
+                    int y = map.getObjectY(groupID, objectID);
 
-	public int getHeight() {
-		return height;
-	}
+                    Vector2f position = new Vector2f(x, y);
 
-	public int getWidth() {
-		return width;
-	}
+                    terrain[x][y] = new BasicGroundTile(position);
+                } else {
+                    // Entity
+                    Entity hp = new HumanPlayer();
+                    entities.add(hp);
+                    positions.put(hp, new Vector2f(0, 0));
+                }
+            }
+        }
+    }
+
+    public void update() {
+        // TODO: for each entity, get the desired velocity, and calculate
+        // an actual velocity based on the entity's location, terrain, and
+        // gravity. Then calculate each entity's new position
+        for (Entity entity : entities) {
+            Vector2f desiredV = entity.update();
+
+        }
+    }
+
+    public Tile getTerrain(int x, int y) {
+        x = x % width; // Automatically handle wrapping around
+        if (y >= 0 && y < height) {
+            return terrain[x][y];
+        } else {
+            System.out.println("TileMap error: called getTerrain with y=" + y + ", but did not meet bounds of [0," + height + ")");
+        }
+        return new BasicBlankTile(new Vector2f(x, y));
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
 }
--- Platformer/src/com/gravity/map/TileMapRenderer.java
+++ Platformer/src/com/gravity/map/TileMapRenderer.java
@@ -3,41 +3,51 @@ package com.gravity.map;
 import java.util.Map;
 
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Vector2f;
 
-import com.google.common.collect.Maps;
 import com.gravity.player.Entity;
 import com.gravity.root.Renderer;
 
 public class TileMapRenderer implements Renderer {
-	private TileMap tileMap;
-	private Map<Entity, Renderer> entityRenderers;
+    private TileMap tileMap;
+    private Map<Entity, Renderer> entityRenderers;
 
-	public TileMapRenderer(TileMap tileMap) {
-		this.tileMap = tileMap;
-		this.entityRenderers = Maps.newHashMap();
-		// TODO: populate entityRenderers - either through construction here
-		// or adding in an arguement
-	}
+    public TileMapRenderer(TileMap tileMap, Map<Entity, Renderer> eAndR) {
+        this.tileMap = tileMap;
+        this.entityRenderers = eAndR;
+    }
 
-	@Override
-	public void render(Graphics g) {
-		int height = tileMap.getHeight();
-		int width = tileMap.getWidth();
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				// TODO: for each tile, render the image in the appropriate area
-			}
-		}
-		for (Entity entity : tileMap.entities) {
-			Vector2f position = tileMap.positions.get(entity);
-			g.pushTransform();
-			g.translate(position.getX(), position.getY());
+    @Override
+    public void render(Graphics g) {
+        int height = tileMap.getHeight();
+        int width = tileMap.getWidth();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // Get the spot and it's image
+                Tile spot = tileMap.getTerrain(x, y);
+                Image img = spot.getImage();
 
-			entityRenderers.get(entity).render(g);
+                // Do maths to figure out where to put it
+                // We do assume here that all tiles are the same width and height
+                int imgWidth = img.getWidth();
+                int imgHeight = img.getHeight();
+                int finalX = imgWidth * x;
+                int finalY = imgHeight * y;
 
-			g.resetTransform();
-			g.popTransform();
-		}
-	}
+                // Put it
+                g.drawImage(img, finalX, finalY);
+            }
+        }
+        for (Entity entity : tileMap.entities) {
+            Vector2f position = tileMap.positions.get(entity);
+            g.pushTransform();
+            g.translate(position.getX(), position.getY());
+
+            entityRenderers.get(entity).render(g);
+
+            g.resetTransform();
+            g.popTransform();
+        }
+    }
 }
