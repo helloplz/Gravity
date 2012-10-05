20709bba012560e0d5d807d5fbb3d6dbd23dac50
Chris Dessonville <cdville@mit.edu>
2012-10-04 22:47:07 -0400

    Cleanup

--- Platformer/src/com/gravity/map/TileWorldRenderer.java
+++ Platformer/src/com/gravity/map/TileWorldRenderer.java
@@ -13,19 +13,11 @@ public class TileWorldRenderer implements Renderer {
 
     @Override
     public void render(Graphics g) {
-        // int height = tileMap.getHeight();
-        // int width = tileMap.getWidth();
 
+        // TiledMap supports easy rendering. Let's use it!
+        // Later we'll need to some how adjust x,y for offset/scrolling
         tileMap.render(g);
-        /*
-         * for (int y = 0; y < height; y++) { for (int x = 0; x < width; x++) { // Get the spot and it's image Tile spot = tileMap.getTerrain(x, y);
-         * Image img = spot.getImage();
-         * 
-         * // Do maths to figure out where to put it // We do assume here that all tiles are the same width and // height int imgWidth =
-         * img.getWidth(); int imgHeight = img.getHeight(); int finalX = imgWidth * x; int finalY = imgHeight * y;
-         * 
-         * // Put it g.drawImage(img, finalX, finalY); } }
-         */
+
         /*
          * TODO: move this to GameEngine. for (Entity entity : tileMap.entities) { Vector2f position = entity.getPosition(); g.pushTransform();
          * g.translate(position.getX(), position.getY());
