09d488340ea9ec37f98fadbf153268163d863e85
Chris Dessonville <cdville@mit.edu>
2012-10-04 22:03:32 -0400

    Adding blank.png, ground.png (still a blank image too) and updating BasicGroundTile

--- Platformer/src/com/gravity/map/BasicGroundTile.java
+++ Platformer/src/com/gravity/map/BasicGroundTile.java
@@ -1,12 +1,20 @@
 package com.gravity.map;
 
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 
 public class BasicGroundTile extends Tile {
 
     static private BasicGroundTile template;
+    static {
+        try {
+            template = new BasicGroundTile(new Image("assets/blank.png"));
+        } catch (SlickException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     static public void loadTileImage(Image image) {
         template = new BasicGroundTile(image);
@@ -20,5 +28,4 @@ public class BasicGroundTile extends Tile {
     public BasicGroundTile(Vector2f position) {
         super(template, position);
     }
-
 }
