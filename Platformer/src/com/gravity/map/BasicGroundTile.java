392516ce8aaa337e82f81b01da150be3d31d33ac
Chris Dessonville <cdville@mit.edu>
2012-10-04 22:01:02 -0400

    Tile loading stuff.

--- Platformer/src/com/gravity/map/BasicGroundTile.java
+++ Platformer/src/com/gravity/map/BasicGroundTile.java
@@ -6,19 +6,19 @@ import org.newdawn.slick.geom.Vector2f;
 
 public class BasicGroundTile extends Tile {
 
-	static private BasicGroundTile template;
+    static private BasicGroundTile template;
 
-	static public void setTileImage(Image image) {
-		template = new BasicGroundTile(image);
-	}
+    static public void loadTileImage(Image image) {
+        template = new BasicGroundTile(image);
+    }
 
-	// Ground tiles are impassible - make the shape so
-	private BasicGroundTile(Image image) {
-		super(new Rectangle(0, 0, 1, 1), image);
-	}
+    // Ground tiles are impassible - make the shape so
+    private BasicGroundTile(Image image) {
+        super(new Rectangle(0, 0, 1, 1), image);
+    }
 
-	public BasicGroundTile(Vector2f position) {
-		super(template, position);
-	}
+    public BasicGroundTile(Vector2f position) {
+        super(template, position);
+    }
 
 }
--- Platformer/src/com/gravity/map/TileWorld.java
+++ Platformer/src/com/gravity/map/TileWorld.java
@@ -10,67 +10,65 @@ import com.gravity.physics.Entity;
 
 public class TileWorld implements GameWorld {
 
-	Tile[][] terrain;
+    private Tile[][] terrain;
 
-	int height;
-	int width;
+    public final int height;
+    public final int width;
 
-	public TileWorld(TiledMap map) {
-		// Get width/height
-		this.width = new Integer(map.getMapProperty("width", "1024"));
-		this.height = new Integer(map.getMapProperty("height", "1024"));
+    public TileWorld(TiledMap map) {
+        // Get width/height
+        this.width = new Integer(map.getMapProperty("width", "1024"));
+        this.height = new Integer(map.getMapProperty("height", "1024"));
 
-		// Assume all objects are currently terrain
-		int groupCount = map.getObjectGroupCount();
-		for (int groupID = 0; groupID < groupCount; groupID++) {
-			int objectCount = map.getObjectCount(groupID);
-			for (int objectID = 0; objectID < objectCount; objectID++) {
-				String type = map.getObjectType(groupID, objectID);
+        this.terrain = new Tile[this.width][this.height];
 
-				if (type.equals("terrain")) {
-					int x = map.getObjectX(groupID, objectID);
-					int y = map.getObjectY(groupID, objectID);
+        // Assume all objects are currently terrain
+        int groupCount = map.getObjectGroupCount();
+        for (int groupID = 0; groupID < groupCount; groupID++) {
+            int objectCount = map.getObjectCount(groupID);
+            for (int objectID = 0; objectID < objectCount; objectID++) {
+                int x = map.getObjectX(groupID, objectID);
+                int y = map.getObjectY(groupID, objectID);
 
-					Vector2f position = new Vector2f(x, y);
+                Vector2f position = new Vector2f(x, y);
 
-					terrain[x][y] = new BasicGroundTile(position);
-				} else {
-					// Entity
-					// TODO: populate entities
-				}
-			}
-		}
-	}
+                terrain[x][y] = new BasicGroundTile(position);
+            }
+        }
+    }
 
-	@Override
-	public List<Shape> getCollisions(Shape shape) {
-		return null;
-	}
+    @Override
+    public List<Shape> getCollisions(Shape shape) {
+        // TODO
+        return null;
+    }
 
-	public Tile getTerrain(int x, int y) {
-		x = x % width; // Automatically handle wrapping around
-		if (y >= 0 && y < height) {
-			return terrain[x][y];
-		} else {
-			System.out.println("TileMap error: called getTerrain with y=" + y
-					+ ", but did not meet bounds of [0," + height + ")");
-		}
-		return new BasicBlankTile(new Vector2f(x, y));
-	}
+    public Tile getTerrain(int x, int y) {
+        x = x % width; // Automatically handle wrapping around
+        if (y >= 0 && y < height) {
+            Tile ret = terrain[x][y];
+            if (ret == null)
+                return new BasicBlankTile(new Vector2f(x, y));
+            return terrain[x][y];
+        } else {
+            System.out.println("TileMap error: called getTerrain with y=" + y + ", but did not meet bounds of [0," + height + ")");
+        }
+        return new BasicBlankTile(new Vector2f(x, y));
+    }
 
-	@Override
-	public int getHeight() {
-		return height;
-	}
+    @Override
+    public int getHeight() {
+        return height;
+    }
 
-	@Override
-	public int getWidth() {
-		return width;
-	}
+    @Override
+    public int getWidth() {
+        return width;
+    }
 
-	@Override
-	public List<Entity> getTerrainEntities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Entity> getTerrainEntities() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
