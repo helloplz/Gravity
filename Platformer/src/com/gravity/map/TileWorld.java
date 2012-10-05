a2cda0ff47a69c61775d013973d9c53c6bcf2290
Chris Dessonville <cdville@mit.edu>
2012-10-05 00:11:00 -0400

    Merge fix

--- Platformer/src/com/gravity/map/TileWorld.java
+++ Platformer/src/com/gravity/map/TileWorld.java
@@ -12,22 +12,22 @@ import com.google.common.collect.Lists;
 import com.gravity.physics.Entity;
 
 public class TileWorld implements GameWorld {
-    
+
     private Tile[][] terrain;
-    
+
     public final int height;
     public final int width;
-    
-    private TiledMap map;
-    
+
+    private final TiledMap map;
+
     public TileWorld(TiledMap map) {
         // Get width/height
         this.width = new Integer(map.getMapProperty("width", "1024"));
         this.height = new Integer(map.getMapProperty("height", "768"));
-        
+
         this.terrain = new Tile[this.width][this.height];
         this.map = map;
-        
+
         // Assume all objects are currently terrain
         /*
          * int groupCount = map.getObjectGroupCount(); for (int groupID = 0; groupID < groupCount; groupID++) { int objectCount =
@@ -41,13 +41,13 @@ public class TileWorld implements GameWorld {
          * System.out.println(x); } }
          */
     }
-    
+
     @Override
     public List<Shape> getCollisions(Shape shape) {
         // TODO
         return null;
     }
-    
+
     public Tile getTerrain(int x, int y) {
         x = x % width; // Automatically handle wrapping around
         if (y >= 0 && y < height) {
@@ -60,23 +60,23 @@ public class TileWorld implements GameWorld {
         }
         return new BasicBlankTile(new Vector2f(x, y));
     }
-    
+
     @Override
     public int getHeight() {
         return height;
     }
-    
+
     @Override
     public int getWidth() {
         return width;
     }
-    
+
     @Override
     public List<Entity> getTerrainEntities() {
         Entity bottom = new TileWorldEntity(new Rectangle(0, 20, 20, 1), this);
         return Lists.newArrayList(bottom);
     }
-    
+
     @Override
     public void render(Graphics g) {
         g.pushTransform();
