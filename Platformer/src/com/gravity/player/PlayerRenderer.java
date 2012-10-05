9e2f2a5586210c198c4d4d11d284993412b6525b
unknown <helloplz@.(none)>
2012-10-04 22:50:33 -0400

    added some dudes

--- Platformer/src/com/gravity/player/PlayerRenderer.java
+++ Platformer/src/com/gravity/player/PlayerRenderer.java
@@ -1,18 +1,29 @@
 package com.gravity.player;
 
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 import com.gravity.root.Renderer;
 
 public class PlayerRenderer implements Renderer {
 	private Player player;
+	private Image playerOneImage;
+	private float x;
+	private float y;
 
 	public PlayerRenderer(Player player) {
 		this.player = player;
+		try {
+            playerOneImage = new Image("./assets/TopHatGuy.png");
+        } catch (SlickException e) {
+            throw new RuntimeException(e);
+        }
+		
 	}
 
 	@Override
 	public void render(Graphics g) {
-		// TODO Auto-generated method stub
+		g.drawImage(playerOneImage, 0, 0 /*this.x, this.y*/);
 	}
 }
