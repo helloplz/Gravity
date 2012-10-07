a299107a4ecfdfe9c14d36320224f1797d362790
fiicere <fiicere@mit.edu>
2012-10-07 14:13:35 -0400

    Reorganized player class

--- Platformer/src/com/gravity/player/Player.java
+++ Platformer/src/com/gravity/player/Player.java
@@ -20,28 +20,27 @@ public class Player implements Entity {
     
     private GravityGameController game;
     
-    // PLAYER STARTING CONSTANTS
-    private final float           JUMP_POWER         = 5;
-    private final float           MOVEMENT_INCREMENT = 10000.0f / 1000f;
-    private float                 MAX_HEALTH         = 10;
-    private float                 MAX_VEL            = 100f / 100f;
-    private float                 VEL_DAMP           = 0.5f;
-    private float                 GRAVITY            = 10.0f / 1000f;
+    // PLAYER STARTING CONSTANTS (Units = pixels/millisecond)
+    private final float JUMP_POWER = 1f;
+    private final float MOVEMENT_INCREMENT = 1f;
+    private float MAX_HEALTH = 10;
+    private float MAX_VEL = 100f;
+    private float VEL_DAMP = 0.5f;
+    private float GRAVITY = 10.0f / 1000f;
     
     // PLAYER CURRENT VALUES
-    private GameWorld             map;
+    private GameWorld map;
     
     // position and magnitude
-    private Vector2f              acceleration       = new Vector2f(0, 0);
-    private Vector2f              oldPosition        = new Vector2f(0, 0);
-    private Vector2f              newPosition        = new Vector2f(0, 0);
-    private Vector2f              velocity           = new Vector2f(0, 0);
-    private Vector2f              facing             = new Vector2f(0, 1);
-    private float                 health;
-    private Shape                 myShape;
+    private Vector2f acceleration = new Vector2f(0, 0);
+    private Vector2f position = new Vector2f(0, 0);
+    private Vector2f velocity = new Vector2f(0, 0);
+    private Vector2f facing = new Vector2f(0, 1);
+    private float health;
+    private Shape myShape;
     
     // GAME STATE STUFF
-    private boolean               onGround           = true;
+    private boolean onGround = true;
     
     public Player(GameWorld map, GravityGameController game) {
         health = MAX_HEALTH;
@@ -51,20 +50,38 @@ public class Player implements Entity {
         this.myShape = new Rectangle(-.5f, -.5f, 1f, 1f);
     }
     
-    public void takeDamage(float damage) {
-        health -= damage;
+    // //////////////////////////////////////////////////////////////////////////
+    // //////////////////////////GET & SET METHODS///////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////
+    public Vector2f getPosition() {
+        return getPosition(0);
     }
     
-    public void heal(float heal) {
-        health += heal;
+    @Override
+    public Vector2f getPosition(int ticks) {
+        return new Vector2f(position.x + (velocity.x * ticks), position.y + (velocity.y * ticks));
     }
+    
+    @Override
+    public Shape getShape(int ticks) {
+        return myShape.transform(Transform.createTranslateTransform(position.x + (velocity.x * ticks), position.y + (velocity.y * ticks)));
+    }
+    
+    @Override
+    public Vector2f getVelocity(int ticks) {
+        return velocity.copy();
+    }
+    
+    // //////////////////////////////////////////////////////////////////////////
+    // //////////////////////////KEY-PRESS METHODS///////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////
     
     /**
      * @param jumping
      *            true if keydown, false if keyup
      */
     public void jump(boolean jumping) {
-        if (jumping) {
+        if (jumping && onGround) {
             velocity.y -= JUMP_POWER;
         }
     }
@@ -90,27 +107,35 @@ public class Player implements Entity {
         }
     }
     
-    public Vector2f getPosition() {
-        return getPosition(0);
-    }
-    
-    @Override
-    public Vector2f getPosition(int ticks) {
-        return new Vector2f(oldPosition.x + (velocity.x * ticks), oldPosition.y + (velocity.y * ticks));
-    }
-    
-    @Override
-    public Shape getShape(int ticks) {
-        return myShape.transform(Transform.createTranslateTransform(oldPosition.x + (velocity.x * ticks), oldPosition.y + (velocity.y * ticks)));
-    }
-    
-    @Override
-    public Vector2f getVelocity(int ticks) {
-        return velocity.copy();
-    }
+    // //////////////////////////////////////////////////////////////////////////
+    // //////////////////////////COLLISION METHODS///////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////
     
     @Override
     public Shape handleCollisions(int ticks, List<Collision> collisions) {
+        Collision first = getFirstCol(collisions);
+        int earliest = first.time;
+        Entity them = first.getOtherEntity(this);
+        
+        if ((them.getShape(earliest) instanceof Rectangle)) {
+            terrainCollision(them, earliest);
+        }
+        // TODO: Write code for non-rectangles
+        return null;
+    }
+    
+    @Override
+    public Shape rehandleCollisions(int ticks, List<Collision> collisions) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    /**
+     * @param list
+     *            of collisions
+     * @return first collision in list
+     */
+    private Collision getFirstCol(List<Collision> collisions) {
         Collision first = null;
         int earliest = -1;
         for (Collision c : collisions) {
@@ -126,63 +151,76 @@ public class Player implements Entity {
         }
         if (earliest == -1) {
             System.out.println("WTF???");
-            return this.getShape(0);
+            return null;
         }
-        Entity me = this;
-        Entity them = first.getOtherEntity(this);
         
-        if ((them.getShape(earliest) instanceof Rectangle)) {
-            newPosition = oldPosition.add(velocity.scale((float) (earliest - (1.0 / 1000))));
-            // If I'm overlapping their xcoord
-            if (me.getShape(earliest + 1).getMaxX() > them.getShape(earliest + 1).getMinX()) {
-                velocity.x = 0;
-            } else if (me.getShape(earliest + 1).getMinX() < them.getShape(earliest + 1).getMaxX()) {
-                velocity.x = 0;
-            }
-            // If I'm overlapping their ycoord
-            else if (me.getShape(earliest + 1).getMinY() < them.getShape(earliest + 1).getMaxY()) {
-                velocity.y = 0;
-            } else if (me.getShape(earliest + 1).getMinY() < them.getShape(earliest + 1).getMaxY()) {
-                velocity.y = 0;
-            }
-        }
-        // TODO: Write code for non-rectangles
-        return null;
-    }
-    
-    @Override
-    public Shape rehandleCollisions(int ticks, List<Collision> collisions) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    @Override
-    public void tick(int millis) {
-        handleMotion(millis);
+        return first;
     }
     
     /**
-     * Makes sure maximum velocities are respected, player position is updated, etc
+     * Handles collision with terrain
      */
-    public void handleMotion(float millis) {
-        oldPosition = newPosition;
-        // Check to see if the player is on the ground
-        isOnGround();
-        // Change the velocity depending on the accelerations
-        acceleration = new Vector2f(0, GRAVITY);
+    public void terrainCollision(Entity collidee, int millis) {
+        position.add(velocity.scale((float) (millis - (1.0 / 1000))));
+        // If I'm overlapping their xcoord
+        if (this.getShape(millis + 1).getMaxX() > collidee.getShape(millis + 1).getMinX()) {
+            velocity.x = 0;
+        } else if (this.getShape(millis + 1).getMinX() < collidee.getShape(millis + 1).getMaxX()) {
+            velocity.x = 0;
+        }
+        // If I'm overlapping their ycoord
+        else if (this.getShape(millis + 1).getMinY() < collidee.getShape(millis + 1).getMaxY()) {
+            velocity.y = 0;
+        } else if (this.getShape(millis + 1).getMinY() < collidee.getShape(millis + 1).getMaxY()) {
+            velocity.y = 0;
+        }
+    }
+    
+    public void takeDamage(float damage) {
+        health -= damage;
+    }
+    
+    public void heal(float heal) {
+        health += heal;
+    }
+    
+    // //////////////////////////////////////////////////////////////////////////
+    // //////////////////////////ON-TICK METHODS/////////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////
+    @Override
+    public void tick(int millis) {
+        isOnGround(millis);
+        updateAcceleration(millis);
+        updateVelocity(millis);
+        updatePosition(millis);
+    }
+    
+    public void updateAcceleration(float millis) {
+        if (onGround) {
+            acceleration.y = 0;
+        } else {
+            acceleration.y = GRAVITY;
+        }
+    }
+    
+    public void updateVelocity(float millis) {
+        // dv = a
         velocity.add(acceleration.copy().scale(millis));
-        // Makes sure velocity does not exceed max velocity
-        if (velocity.length() > MAX_VEL * millis)
+        
+        // velocity < maxVel
+        if (velocity.length() > MAX_VEL * millis) {
             velocity.scale(MAX_VEL * millis / velocity.length());
-        // Updates position to reflect velocity
-        newPosition = oldPosition.add(velocity);
-        System.out.println("oldPosition = " + oldPosition.x);
+        }
+    }
+    
+    public void updatePosition(float millis) {
+        position.add(velocity.copy().scale(millis));
     }
     
     /*
      * Sets onGround depending on if the player is on the ground or not
      */
-    public void isOnGround() {
+    public void isOnGround(float millis) {
         // TODO
     }
     
