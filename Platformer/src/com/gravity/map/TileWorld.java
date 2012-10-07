14e604c76569a30eb62018b8296296e2d4dd9303
pgruevski <predrag_gruevski@yahoo.com>
2012-10-07 15:23:27 -0400

    Fixed rectangle-related player and tile bugs.

--- Platformer/src/com/gravity/map/TileWorld.java
+++ Platformer/src/com/gravity/map/TileWorld.java
@@ -15,23 +15,23 @@ import com.gravity.physics.Entity;
 
 public class TileWorld implements GameWorld {
     private Tile[][] terrain;
-
+    
     public final int height;
     public final int width;
-
+    
     private List<Entity> entities;
     private Map<Shape, Entity> touchingBoxes;
-
+    
     private TiledMap map;
-
+    
     public TileWorld(TiledMap map) {
         // Get width/height
         this.width = new Integer(map.getMapProperty("width", "1024"));
         this.height = new Integer(map.getMapProperty("height", "768"));
-
+        
         this.terrain = new Tile[this.width][this.height];
         this.map = map;
-
+        
         // Assume all objects are currently terrain
         /*
          * int groupCount = map.getObjectGroupCount(); for (int groupID = 0; groupID < groupCount; groupID++) { int objectCount =
@@ -44,15 +44,15 @@ public class TileWorld implements GameWorld {
          * 
          * System.out.println(x); } }
          */
-
+        
         touchingBoxes = Maps.newHashMap();
-        Entity bottom = new TileWorldEntity(new Rectangle(0, 20, 20, 1), this);
-        Rectangle bound = new Rectangle(0, 20, 20, 1);
+        Entity bottom = new TileWorldEntity(new Rectangle(0, 20, 20, 2), this);
+        Rectangle bound = new Rectangle(0, 20, 20, 2);
         bound.grow(.1f, .1f);
         touchingBoxes.put(bound, bottom);
         entities = Lists.newArrayList(bottom);
     }
-
+    
     @Override
     public List<Entity> getCollisions(Shape shape) {
         List<Entity> collisions = Lists.newArrayList();
@@ -63,7 +63,7 @@ public class TileWorld implements GameWorld {
         }
         return collisions;
     }
-
+    
     @Override
     public List<Entity> getTouching(Shape shape) {
         List<Entity> touches = Lists.newArrayList();
@@ -74,7 +74,7 @@ public class TileWorld implements GameWorld {
         }
         return touches;
     }
-
+    
     public Tile getTerrain(int x, int y) {
         x = x % width; // Automatically handle wrapping around
         if (y >= 0 && y < height) {
@@ -87,22 +87,22 @@ public class TileWorld implements GameWorld {
         }
         return new BasicBlankTile(new Vector2f(x, y));
     }
-
+    
     @Override
     public int getHeight() {
         return height;
     }
-
+    
     @Override
     public int getWidth() {
         return width;
     }
-
+    
     @Override
     public List<Entity> getTerrainEntities() {
         return entities;
     }
-
+    
     @Override
     public void render(Graphics g) {
         g.pushTransform();
--- Platformer/src/com/gravity/root/GameplayState.java
+++ Platformer/src/com/gravity/root/GameplayState.java
@@ -11,77 +11,76 @@ import org.newdawn.slick.tiled.TiledMap;
 import com.gravity.gameplay.GravityGameController;
 import com.gravity.map.TileWorld;
 import com.gravity.map.TileWorldRenderer;
+import com.gravity.physics.CollisionEngine;
 import com.gravity.player.Player;
 import com.gravity.player.PlayerKeyboardController;
 import com.gravity.player.PlayerRenderer;
 
-public class GameplayState extends BasicGameState implements
-		GravityGameController {
-
-	@Override
-	public int getID() {
-		return 1;
-	}
-
-	private TileWorld map;
-	private Player playerA, playerB;
-	private TileWorldRenderer rendererMap;
-	private PlayerRenderer rendererA, rendererB;
-	private PlayerKeyboardController controllerA, controllerB;
-
-	@Override
-	public void init(GameContainer container, StateBasedGame game)
-			throws SlickException {
-		resetState();
-	}
-
-	public void resetState() throws SlickException {
-		map = new TileWorld(new TiledMap("assets/test2.tmx"));
-		playerA = new Player(map, this);
-		playerB = new Player(map, this);
-		rendererMap = new TileWorldRenderer(map);
-		rendererA = new PlayerRenderer(playerA);
-		rendererB = new PlayerRenderer(playerB);
-		controllerA = new PlayerKeyboardController(playerA)
-				.setLeft(Input.KEY_A).setRight(Input.KEY_D)
-				.setJump(Input.KEY_W).setMisc(Input.KEY_S);
-		controllerB = new PlayerKeyboardController(playerB)
-				.setLeft(Input.KEY_LEFT).setRight(Input.KEY_RIGHT)
-				.setJump(Input.KEY_UP).setMisc(Input.KEY_DOWN);
-	}
-
-	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g)
-			throws SlickException {
-		// TODO call the render stack
-		rendererMap.render(g);
-		rendererB.render(g);
-	}
-
-	@Override
-	public void update(GameContainer container, StateBasedGame game, int delta)
-			throws SlickException {
-		playerB.tick(delta);
-		// TODO update on CollisionEngine and other players
-
-	}
-
-	@Override
-	public void keyPressed(int key, char c) {
-		if (!controllerA.handleKeyPress(key)) {
-			controllerB.handleKeyPress(key);
-		}
-	}
-
-	@Override
-	public void keyReleased(int key, char c) {
-		if (!controllerA.handleKeyRelease(key)) {
-			controllerB.handleKeyRelease(key);
-		}
-	}
-
-	@Override
-	public void playerDies(Player player) {
-		// TODO Auto-generated method stub
-	}
+public class GameplayState extends BasicGameState implements GravityGameController {
+    
+    @Override
+    public int getID() {
+        return 1;
+    }
+    
+    private TileWorld map;
+    private Player playerA, playerB;
+    private TileWorldRenderer rendererMap;
+    private PlayerRenderer rendererA, rendererB;
+    private PlayerKeyboardController controllerA, controllerB;
+    private CollisionEngine collisions;
+    
+    @Override
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+        resetState();
+    }
+    
+    public void resetState() throws SlickException {
+        map = new TileWorld(new TiledMap("assets/test2.tmx"));
+        playerA = new Player(map, this);
+        playerB = new Player(map, this);
+        rendererMap = new TileWorldRenderer(map);
+        rendererA = new PlayerRenderer(playerA);
+        rendererB = new PlayerRenderer(playerB);
+        controllerA = new PlayerKeyboardController(playerA).setLeft(Input.KEY_A).setRight(Input.KEY_D).setJump(Input.KEY_W).setMisc(Input.KEY_S);
+        controllerB = new PlayerKeyboardController(playerB).setLeft(Input.KEY_LEFT).setRight(Input.KEY_RIGHT).setJump(Input.KEY_UP)
+                .setMisc(Input.KEY_DOWN);
+        collisions = new CollisionEngine(map);
+        // collisions.addEntity(playerA);
+        collisions.addEntity(playerB);
+    }
+    
+    @Override
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+        // TODO call the render stack
+        rendererMap.render(g);
+        rendererB.render(g);
+    }
+    
+    @Override
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+        collisions.update(delta);
+        // playerB.tick(delta);
+        // TODO update on CollisionEngine and other players
+        
+    }
+    
+    @Override
+    public void keyPressed(int key, char c) {
+        if (!controllerA.handleKeyPress(key)) {
+            controllerB.handleKeyPress(key);
+        }
+    }
+    
+    @Override
+    public void keyReleased(int key, char c) {
+        if (!controllerA.handleKeyRelease(key)) {
+            controllerB.handleKeyRelease(key);
+        }
+    }
+    
+    @Override
+    public void playerDies(Player player) {
+        // TODO Auto-generated method stub
+    }
 }
