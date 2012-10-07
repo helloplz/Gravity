3eafc1697c61a959d0a1fbe373d9a7ca7941ed6f
unknown <helloplz@.(none)>
2012-10-07 16:57:49 -0400

    made predrag happier by making my code less shitty.  Also chris

--- Platformer/src/com/gravity/player/PlayerRenderer.java
+++ Platformer/src/com/gravity/player/PlayerRenderer.java
@@ -39,53 +39,44 @@ public class PlayerRenderer implements Renderer {
     private Image lastImage;
     private List<Image> runningBunny;
     private List<Image> runningBackBunny;
+    private List<Image> jumpingBunny;
+    private List<Image> JumpingBackBunny;
     private int tweener;
 
     public PlayerRenderer(Player player) {
         this.player = player;
         try {
+            runningBunny = Lists.newArrayList();
+            runningBackBunny = Lists.newArrayList();
+            jumpingBunny = Lists.newArrayList();
+
             bunnyPlayer = new Image("./assets/BunnyAssets/bunnyStand.png");
-            bunnyPlayerRun1 = new Image("./assets/BunnyAssets/bunnyRun1.png");
-            bunnyPlayerRun2 = new Image("./assets/BunnyAssets/bunnyRun2.png");
-            bunnyPlayerRun3 = new Image("./assets/BunnyAssets/bunnyRun3.png");
-            bunnyPlayerRun4 = new Image("./assets/BunnyAssets/bunnyRun4.png");
-            bunnyPlayerRun5 = new Image("./assets/BunnyAssets/bunnyRun5.png");
-            bunnyPlayerRun6 = new Image("./assets/BunnyAssets/bunnyRun6.png");
-            bunnyPlayerJump1 = new Image("./assets/BunnyAssets/bunnyJump1.png");
-            bunnyPlayerJump2 = new Image("./assets/BunnyAssets/bunnyJump2.png");
-            bunnyPlayerJump3 = new Image("./assets/BunnyAssets/bunnyJump3.png");
-            bunnyPlayerJump4 = new Image("./assets/BunnyAssets/bunnyJump4.png");
-            bunnyPlayerJump5 = new Image("./assets/BunnyAssets/bunnyJump5.png");
-            bunnyPlayerJump6 = new Image("./assets/BunnyAssets/bunnyJump6.png");
+            runningBunny.add(bunnyPlayerRun1 = new Image("./assets/BunnyAssets/bunnyRun1.png"));
+            runningBunny.add(bunnyPlayerRun2 = new Image("./assets/BunnyAssets/bunnyRun2.png"));
+            runningBunny.add(bunnyPlayerRun3 = new Image("./assets/BunnyAssets/bunnyRun3.png"));
+            runningBunny.add(bunnyPlayerRun4 = new Image("./assets/BunnyAssets/bunnyRun4.png"));
+            runningBunny.add(bunnyPlayerRun5 = new Image("./assets/BunnyAssets/bunnyRun5.png"));
+            runningBunny.add(bunnyPlayerRun6 = new Image("./assets/BunnyAssets/bunnyRun6.png"));
+            jumpingBunny.add(bunnyPlayerJump1 = new Image("./assets/BunnyAssets/bunnyJump1.png"));
+            jumpingBunny.add(bunnyPlayerJump2 = new Image("./assets/BunnyAssets/bunnyJump2.png"));
+            jumpingBunny.add(bunnyPlayerJump3 = new Image("./assets/BunnyAssets/bunnyJump3.png"));
+            jumpingBunny.add(bunnyPlayerJump4 = new Image("./assets/BunnyAssets/bunnyJump4.png"));
+            jumpingBunny.add(bunnyPlayerJump5 = new Image("./assets/BunnyAssets/bunnyJump5.png"));
+            jumpingBunny.add(bunnyPlayerJump6 = new Image("./assets/BunnyAssets/bunnyJump6.png"));
             bunnyPlayerDuck1 = new Image("./assets/BunnyAssets/bunnyDuck1.png");
             bunnyPlayerDuck2 = new Image("./assets/BunnyAssets/bunnyDuck2.png");
             bunnyPlayerDuck3 = new Image("./assets/BunnyAssets/bunnyDuck3.png");
             bunnyPlayerBack = new Image("./assets/BunnyAssets/bunnyStandBack.png");
-            bunnyPlayerRunBack1 = new Image("./assets/BunnyAssets/bunnyRunBack1.png");
-            bunnyPlayerRunBack2 = new Image("./assets/BunnyAssets/bunnyRunBack2.png");
-            bunnyPlayerRunBack3 = new Image("./assets/BunnyAssets/bunnyRunBack3.png");
-            bunnyPlayerRunBack4 = new Image("./assets/BunnyAssets/bunnyRunBack4.png");
-            bunnyPlayerRunBack5 = new Image("./assets/BunnyAssets/bunnyRunBack5.png");
-            bunnyPlayerRunBack6 = new Image("./assets/BunnyAssets/bunnyRunBack6.png");
+            runningBackBunny.add(bunnyPlayerRunBack1 = new Image("./assets/BunnyAssets/bunnyRunBack1.png"));
+            runningBackBunny.add(bunnyPlayerRunBack2 = new Image("./assets/BunnyAssets/bunnyRunBack2.png"));
+            runningBackBunny.add(bunnyPlayerRunBack3 = new Image("./assets/BunnyAssets/bunnyRunBack3.png"));
+            runningBackBunny.add(bunnyPlayerRunBack4 = new Image("./assets/BunnyAssets/bunnyRunBack4.png"));
+            runningBackBunny.add(bunnyPlayerRunBack5 = new Image("./assets/BunnyAssets/bunnyRunBack5.png"));
+            runningBackBunny.add(bunnyPlayerRunBack6 = new Image("./assets/BunnyAssets/bunnyRunBack6.png"));
 
         } catch (SlickException e) {
             throw new RuntimeException(e);
         }
-        runningBunny = Lists.newArrayList();
-        runningBunny.add(bunnyPlayerRun1);
-        runningBunny.add(bunnyPlayerRun2);
-        runningBunny.add(bunnyPlayerRun3);
-        runningBunny.add(bunnyPlayerRun4);
-        runningBunny.add(bunnyPlayerRun5);
-        runningBunny.add(bunnyPlayerRun6);
-
-        runningBackBunny = Lists.newArrayList();
-        runningBackBunny.add(bunnyPlayerRunBack1);
-        runningBackBunny.add(bunnyPlayerRunBack2);
-        runningBackBunny.add(bunnyPlayerRunBack3);
-        runningBackBunny.add(bunnyPlayerRunBack4);
-        runningBackBunny.add(bunnyPlayerRunBack5);
-        runningBackBunny.add(bunnyPlayerRunBack6);
     }
 
     @Override
@@ -94,31 +85,22 @@ public class PlayerRenderer implements Renderer {
         this.y = player.getPosition().y;
         if (tweener % 4 == 0) {
             if (lastImage == null) {
-                g.drawImage(bunnyPlayer, this.x, this.y);
                 lastImage = bunnyPlayer;
-            } else if (player.getVelocity(0).x > 0) {
-                g.drawImage((runningBunny.get(runningBunny.indexOf(lastImage) + 1)), this.x, this.y);
-                lastImage = runningBunny.get(runningBunny.indexOf(lastImage) + 1);
-                if (lastImage == bunnyPlayerRun6) {
-                    lastImage = bunnyPlayerRun1;
-                }
-            } else if (player.getVelocity(0).x < 0) {
-                g.drawImage((runningBackBunny.get(runningBackBunny.indexOf(lastImage) + 1)), this.x, this.y);
-                lastImage = runningBackBunny.get(runningBackBunny.indexOf(lastImage) + 1);
-                if (lastImage == bunnyPlayerRunBack6) {
-                    lastImage = bunnyPlayerRunBack1;
-                }
-            } else {
-                g.drawImage(bunnyPlayer, this.x, this.y);
-                lastImage = bunnyPlayer;
-
             }
-
+        } else if (player.getVelocity(0).x > 0) {
+            lastImage = runningBunny.get(runningBunny.indexOf(lastImage) + 1);
+            if (lastImage == bunnyPlayerRun6) {
+                lastImage = bunnyPlayerRun1;
+            }
+        } else if (player.getVelocity(0).x < 0) {
+            lastImage = runningBackBunny.get(runningBackBunny.indexOf(lastImage) + 1);
+            if (lastImage == bunnyPlayerRunBack6) {
+                lastImage = bunnyPlayerRunBack1;
+            }
+        } else {
+            lastImage = bunnyPlayer;
         }
-
-        else {
-            g.drawImage(lastImage, this.x, this.y);
-        }
+        g.drawImage(lastImage, this.x, this.y);
         tweener++;
     }
 }
