6c184891ed2edd0b370985da6ddbc08949ebd93d
dxiao <dxiao@mit.edu>
2012-10-10 16:20:18 -0400

    Moved collision detection stuff into collisionEngine

--- Platformer/src/com/gravity/physics/CollisionEngine.java
+++ Platformer/src/com/gravity/physics/CollisionEngine.java
@@ -1,25 +1,31 @@
 package com.gravity.physics;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Vector2f;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.gravity.map.GameWorld;
 
 public class CollisionEngine {
     
-    private final GameWorld gameMap;
-    private final List<Entity> entities;
-    private final List<Entity> terrain;
-    private final List<Entity> spikes;
+    private final GameWorld    gameMap;
+    private final Set<Entity>  entities;
+    private final List<Entity> terrainCallColls;
+    private final List<Entity> terrainNoCalls;
     
     public CollisionEngine(GameWorld map) {
         gameMap = map;
-        entities = Lists.newArrayList();
-        terrain = map.getTerrainEntities();
-        spikes = Lists.newArrayList();
+        entities = Sets.newIdentityHashSet();
+        terrainNoCalls = map.getTerrainEntities();
+        terrainCallColls = Lists.newArrayList();
     }
     
     public boolean addEntity(Entity entity) {
@@ -38,64 +44,21 @@ public class CollisionEngine {
         // until everything works out. Then update everybody
         Preconditions.checkArgument(ticks > 0, "Number of ticks must be positive: ticks=" + ticks);
         
+        // Map<Entity, List<Collision>> collisions;
+        
         int increment = ticks < PARTS_PER_TICK ? 1 : ticks / PARTS_PER_TICK;
-        int entitiesSize = entities.size();
-        int terrainSize = terrain.size();
-        int i, j;
+        Map<Entity, List<Collision>> collisions;
         for (int time = increment; time <= ticks; time += increment) {
             // terrain does not change, not calling update on it
-            for (i = 0; i < entitiesSize; i++) {
-                Shape a = entities.get(i).getShape(increment);
-                List<Collision> collisions = Lists.newArrayList();
-                Shape b;
-                for (j = 0; j < terrainSize; j++) {
-                    b = terrain.get(j).getShape(increment);
-                    if (a.intersects(b)) {
-                        collisions.add(new Collision(entities.get(i), terrain.get(j), increment, EntityPhysics.collisionPoint(a, b)));
-                    }
+            collisions = computeCollisions(increment);
+            if (!collisions.isEmpty()) {
+                for (Entry<Entity, List<Collision>> c : collisions.entrySet()) {
+                    c.getKey().handleCollisions(increment, c.getValue());
                 }
-                
-                for (j = 0; j < entitiesSize; j++) {
-                    if (i != j) {
-                        b = entities.get(j).getShape(increment);
-                        if (a.intersects(b)) {
-                            collisions.add(new Collision(entities.get(i), entities.get(j), increment, EntityPhysics.collisionPoint(a, b)));
-                        }
-                    }
-                }
-                if (!collisions.isEmpty()) {
-                    a = entities.get(i).handleCollisions(increment, collisions);
-                    
-                    while (!collisions.isEmpty()) {
-                        collisions.clear();
-                        for (j = 0; j < terrainSize; j++) {
-                            b = terrain.get(j).getShape(increment);
-                            if (a.intersects(b)) {
-                                collisions.add(new Collision(entities.get(i), terrain.get(j), increment, EntityPhysics.collisionPoint(a, b)));
-                            }
-                        }
-                        
-                        for (j = 0; j < entitiesSize; j++) {
-                            if (i != j) {
-                                b = entities.get(j).getShape(increment);
-                                if (a.intersects(b)) {
-                                    collisions.add(new Collision(entities.get(i), entities.get(j), increment, EntityPhysics.collisionPoint(a, b)));
-                                }
-                            }
-                        }
-                        if (!collisions.isEmpty()) {
-                            a = entities.get(i).rehandleCollisions(increment, collisions);
-                        }
-                    }
-                }
-            }
-            for (Entity spike : spikes) {
-                Shape b = spike.getShape(increment);
-                for (Entity player : entities) {
-                    Shape a = player.getShape(increment);
-                    if (a.intersects(b)) {
-                        spike.handleCollisions(increment,
-                                Lists.newArrayList(new Collision(spike, player, increment, EntityPhysics.collisionPoint(a, b))));
+                collisions = computeCollisions(increment);
+                if (!collisions.isEmpty()) { // TODO: should this be if or while?
+                    for (Entry<Entity, List<Collision>> c : collisions.entrySet()) {
+                        c.getKey().rehandleCollisions(increment, c.getValue());
                     }
                 }
             }
@@ -104,5 +67,196 @@ public class CollisionEngine {
             }
         }
         
+    }
+    
+    Map<Entity, List<Collision>> computeCollisions(int ticks) {
+        int entitiesSize = entities.size();
+        int terrainSize = terrainNoCalls.size();
+        int i, j;
+        Map<Entity, List<Collision>> collisions = Maps.newHashMap();
+        Collision coll;
+        for (Entity a : entities) {
+            for (Entity b : terrainNoCalls) {
+                coll = collisionLines(a, b, ticks);
+                if (coll != null) {
+                    addCollisionToMap(collisions, a, coll);
+                }
+            }
+        }
+        for (Entity a : entities) {
+            for (Entity b : terrainCallColls) {
+                coll = collisionLines(a, b, ticks);
+                if (coll != null) {
+                    addCollisionToMap(collisions, a, coll);
+                    addCollisionToMap(collisions, b, coll);
+                }
+            }
+        }
+        for (Entity a : entities) {
+            for (Entity b : entities) {
+                if (a != b) {
+                    coll = collisionLines(a, b, ticks);
+                    if (coll != null) {
+                        addCollisionToMap(collisions, a, coll);
+                        addCollisionToMap(collisions, b, coll);
+                    }
+                }
+            }
+        }
+        
+        return collisions;
+    }
+    
+    private void addCollisionToMap(Map<Entity, List<Collision>> colls, Entity ent, Collision coll) {
+        if (colls.containsKey(ent)) {
+            colls.get(ent).add(coll);
+        } else {
+            colls.put(ent, Lists.newArrayList(coll));
+        }
+    }
+    
+    static boolean isIntersecting(float[] p1, float[] p2, float[] p3, float[] p4) {
+        double unknownA, unknownB;
+        /*
+         * Intersection formula used: (x4 - x3)(y1 - y3) - (y4 - y3)(x1 - x3) UA = --------------------------------------- (y4 - y3)(x2 - x1) - (x4 -
+         * x3)(y2 - y1)
+         * 
+         * (x2 - x1)(y1 - y3) - (y2 - y1)(x1 - x3) UB = --------------------------------------- (y4 - y3)(x2 - x1) - (x4 - x3)(y2 - y1)
+         * 
+         * if UA and UB are both between 0 and 1 then the lines intersect.
+         * 
+         * Source: http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
+         */
+        unknownA = (((p4[0] - p3[0]) * (double) (p1[1] - p3[1])) - ((p4[1] - p3[1]) * (p1[0] - p3[0])))
+                / (((p4[1] - p3[1]) * (p2[0] - p1[0])) - ((p4[0] - p3[0]) * (p2[1] - p1[1])));
+        unknownB = (((p2[0] - p1[0]) * (double) (p1[1] - p3[1])) - ((p2[1] - p1[1]) * (p1[0] - p3[0])))
+                / (((p4[1] - p3[1]) * (p2[0] - p1[0])) - ((p4[0] - p3[0]) * (p2[1] - p1[1])));
+        
+        if (unknownA >= 0 && unknownA <= 1 && unknownB >= 0 && unknownB <= 1) {
+            return true;
+        }
+        return false;
+    }
+    
+    static Collision collisionLines(Entity entityA, Entity entityB, int time) {
+        // @formatter:off
+        /*
+         * Intersection formula used:
+         *      (x4 - x3)(y1 - y3) - (y4 - y3)(x1 - x3)
+         * UA = ---------------------------------------
+         *      (y4 - y3)(x2 - x1) - (x4 - x3)(y2 - y1)
+         *      
+         *      (x2 - x1)(y1 - y3) - (y2 - y1)(x1 - x3)
+         * UB = ---------------------------------------
+         *      (y4 - y3)(x2 - x1) - (x4 - x3)(y2 - y1)
+         *      
+         * if UA and UB are both between 0 and 1 then the lines intersect.
+         * 
+         * Source: http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
+         */
+        // @formatter:on
+        
+        Shape a = entityA.getShape(time);
+        Shape b = entityB.getShape(time);
+        Map<Integer, List<Integer>> aCollisions = Maps.newHashMap();
+        Map<Integer, List<Integer>> bCollisions = Maps.newHashMap();
+        float points[] = a.getPoints(); // (x3, y3) and (x4, y4)
+        float thatPoints[] = b.getPoints(); // (x1, y1) and (x2, y2)
+        int length = points.length;
+        int thatLength = thatPoints.length;
+        double unknownA;
+        double unknownB;
+        
+        if (!a.closed()) {
+            length -= 2;
+        }
+        if (!b.closed()) {
+            thatLength -= 2;
+        }
+        
+        // x1 = thatPoints[j]
+        // x2 = thatPoints[j + 2]
+        // y1 = thatPoints[j + 1]
+        // y2 = thatPoints[j + 3]
+        // x3 = points[i]
+        // x4 = points[i + 2]
+        // y3 = points[i + 1]
+        // y4 = points[i + 3]
+        for (int i = 0; i < length; i += 2) {
+            int iNext = i + 2;
+            if (iNext >= points.length) {
+                iNext = 0;
+            }
+            
+            for (int j = 0; j < thatLength; j += 2) {
+                int jNext = j + 2;
+                if (jNext >= thatPoints.length) {
+                    jNext = 0;
+                }
+                
+                unknownA = (((points[iNext] - points[i]) * (double) (thatPoints[j + 1] - points[i + 1])) - ((points[iNext + 1] - points[i + 1]) * (thatPoints[j] - points[i])))
+                        / (((points[iNext + 1] - points[i + 1]) * (thatPoints[jNext] - thatPoints[j])) - ((points[iNext] - points[i]) * (thatPoints[jNext + 1] - thatPoints[j + 1])));
+                unknownB = (((thatPoints[jNext] - thatPoints[j]) * (double) (thatPoints[j + 1] - points[i + 1])) - ((thatPoints[jNext + 1] - thatPoints[j + 1]) * (thatPoints[j] - points[i])))
+                        / (((points[iNext + 1] - points[i + 1]) * (thatPoints[jNext] - thatPoints[j])) - ((points[iNext] - points[i]) * (thatPoints[jNext + 1] - thatPoints[j + 1])));
+                
+                if (unknownA >= 0 && unknownA <= 1 && unknownB >= 0 && unknownB <= 1) {
+                    if (aCollisions.containsKey(i / 2)) {
+                        aCollisions.get(i / 2).add(j / 2);
+                    } else {
+                        aCollisions.put(i / 2, Lists.newArrayList(j / 2));
+                    }
+                    if (bCollisions.containsKey(j / 2)) {
+                        bCollisions.get(j / 2).add(i / 2);
+                    } else {
+                        bCollisions.put(j / 2, Lists.newArrayList(i / 2));
+                    }
+                }
+            }
+        }
+        
+        // if true, then bCollisions better be empty as well.
+        if (!aCollisions.isEmpty()) {
+            Set<Integer> aPoints = Sets.newHashSet();
+            Set<Integer> bPoints = Sets.newHashSet();
+            getIntersectPoints(a, b, aCollisions, aPoints, bPoints);
+            getIntersectPoints(b, a, bCollisions, bPoints, aPoints);
+            return new Collision(entityA, entityB, time, aPoints, bPoints);
+        }
+        
+        return null;
+    }
+    
+    private static void getIntersectPoints(Shape a, Shape b, Map<Integer, List<Integer>> aCollisions, Set<Integer> aPoints, Set<Integer> bPoints) {
+        int aLength = a.getPointCount();
+        int bLength = b.getPointCount();
+        float[] centerA = a.getCenter();
+        float[] centerB = b.getCenter();
+        Vector2f centerV = new Vector2f(centerB[0] - centerA[0], centerB[1] - centerA[1]);
+        for (int aLine : aCollisions.keySet()) {
+            List<Integer> bLines = aCollisions.get(aLine);
+            for (int bLine : bLines) {
+                List<Integer> aAdj;
+                if (bLines.contains((bLine + 1) % bLength)) {
+                    aPoints.add(aLine);
+                    aPoints.add((aLine + 1) % aLength);
+                    bPoints.add((bLine + 1) % bLength);
+                } else if (bLines.contains((bLine + 2) % bLength)) {
+                    if (isIntersecting(centerB, b.getPoint((bLine + 1) % bLength), a.getPoint(aLine), a.getPoint((aLine + 1) % aLength))) {
+                        aPoints.add(aLine);
+                        aPoints.add((aLine + 1) % aLength);
+                        bPoints.add((bLine + 1) % bLength);
+                        bPoints.add((bLine + 2) % bLength);
+                    }
+                } else if ((aAdj = aCollisions.get((aLine + 1) % aLength)) != null) {
+                    if (aAdj.contains((bLine + 1) % bLength)) {
+                        aPoints.add((aLine + 1) % aLength);
+                        bPoints.add((bLine + 1) % bLength);
+                    } else if (aAdj.contains((bLine + bLength - 1) % bLength)) {
+                        aPoints.add((aLine + 1) % aLength);
+                        bPoints.add(bLine);
+                    }
+                }
+            }
+        }
     }
 }
--- Platformer/test/com/gravity/physics/CollisionEngineTest.java
+++ Platformer/test/com/gravity/physics/CollisionEngineTest.java
@@ -1,4 +1,4 @@
-package com.gravity.player;
+package com.gravity.physics;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -6,17 +6,15 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 import com.gravity.map.TileWorldEntity;
-import com.gravity.physics.Collision;
-import com.gravity.physics.EntityPhysics;
 
-public class EntityPhysicsTest {
+public class CollisionEngineTest {
     
     @Test
     public void testCornerCollisions() {
         Shape a = new Rectangle(0, 0, 10, 5);
         Shape b = new Rectangle(8, 3, 10, 5);
         
-        Collision collision = EntityPhysics.collisionLines(new TileWorldEntity(a, null), new TileWorldEntity(b, null), 0);
+        Collision collision = CollisionEngine.collisionLines(new TileWorldEntity(a, null), new TileWorldEntity(b, null), 0);
         Assert.assertArrayEquals(new Integer[] { 2 }, collision.collisionsA.toArray());
         Assert.assertArrayEquals(new Integer[] { 0 }, collision.collisionsB.toArray());
     }
@@ -26,7 +24,7 @@ public class EntityPhysicsTest {
         Shape a = new Rectangle(0, 0, 10, 5);
         Shape b = new Rectangle(0, 3, 10, 5);
         
-        Collision collision = EntityPhysics.collisionLines(new TileWorldEntity(a, null), new TileWorldEntity(b, null), 0);
+        Collision collision = CollisionEngine.collisionLines(new TileWorldEntity(a, null), new TileWorldEntity(b, null), 0);
         Assert.assertArrayEquals(new Integer[] { 2, 3 }, collision.collisionsA.toArray());
         Assert.assertArrayEquals(new Integer[] { 0, 1 }, collision.collisionsB.toArray());
     }
@@ -36,7 +34,7 @@ public class EntityPhysicsTest {
         Shape a = new Rectangle(0, 0, 10, 5);
         Shape b = new Rectangle(-2, 4, 14, 9);
         
-        Collision collision = EntityPhysics.collisionLines(new TileWorldEntity(a, null), new TileWorldEntity(b, null), 0);
+        Collision collision = CollisionEngine.collisionLines(new TileWorldEntity(a, null), new TileWorldEntity(b, null), 0);
         Assert.assertArrayEquals(new Integer[] { 2, 3 }, collision.collisionsA.toArray());
         Assert.assertArrayEquals(new Integer[] { 0, 1 }, collision.collisionsB.toArray());
     }
